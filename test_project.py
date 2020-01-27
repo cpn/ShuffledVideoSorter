82750a04c8a6bbeea098a199ce603d23a8de5e0b
cpn <stirbuciprian@gmail.com>
2020-01-27 16:51:46 +0100

    setUp function in unit test did not work as expected

--- test_project.py
+++ test_project.py
@@ -11,28 +11,29 @@ class TestImageSorter(unittest.TestCase):
         image = np.random.rand(50,50)
         pyplot.imsave("tempInput/image_1.jpg", image)
         os.mkdir("tempOutput")
-        return ImageSorter("tempInput", "tempOutput")
+        self.ImageSorter = ImageSorter("tempInput", "tempOutput")
 
     def tearDown(self):
         os.remove("tempInput/image_1.jpg")
         os.rmdir("tempInput")
         os.rmdir("tempOutput")
+        del self.ImageSorter
 
     def test_compare_two_images_L2(self):
         image_1 = np.random.rand(50,50)
         image_2 = np.random.rand(50,50)
-        score_1 = ImageSorter.compare_two_images_L2(image_1, image_1)
-        score_2 = ImageSorter.compare_two_images_L2(image_1, image_2)
+        score_1 = self.ImageSorter.compare_two_images_L2(image_1, image_1)
+        score_2 = self.ImageSorter.compare_two_images_L2(image_1, image_2)
         self.assertTrue(score_1<score_2)
 
     def test_compare_two_images_cosine(self):
         image_1 = np.random.rand(50,50)
         image_2 = np.random.rand(50,50)
-        score_1 = ImageSorter.compare_two_images_cosine(image_1, image_1)
-        score_2 = ImageSorter.compare_two_images_cosine(image_1, image_2)
+        score_1 = self.ImageSorter.compare_two_images_cosine(image_1, image_1)
+        score_2 = self.ImageSorter.compare_two_images_cosine(image_1, image_2)
         self.assertTrue(score_1<score_2)
 
     def test_substract_background(self):
         image_1 = np.random.rand(50,50)
-        mask = ImageSorter.substract_background([image_1])
+        mask = self.ImageSorter.substract_background([image_1])
         self.assertTrue(not np.any(mask))
